0b4bae54ea6f3e3f2ced16c4099d113159b34226
Camargo760 <camargo_co@outlook.com>
2025-01-01 06:49:04 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
           </Link>
           {session ? (
             <>
-              {session.user.email === process.env.ADMIN_EMAIL && (
+              {session.user.email === 'admin@example.com' && (
                 <Link href="/admin" className="nav-link">
                   Admin
                 </Link>
