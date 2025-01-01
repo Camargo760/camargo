e2c3013ccc837a33cd0cb1933b052a2e00a312a2
Camargo760 <camargo_co@outlook.com>
2025-01-02 01:37:23 +0530

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -25,7 +25,7 @@ export default function Admin() {
 
   useEffect(() => {
     if (status === 'loading') return;
-    if (!session || session.user.email !== 'admin@example.com') {
+    if (!session || session.user.email !== 'camargo_co@outlook.com') {
       router.push('/');
     } else {
       fetchProducts();
