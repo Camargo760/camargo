4eb959d669016ea035b732c86a4856deb94040fd
Camargo760 <camargo_co@outlook.com>
2025-04-05 05:29:06 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -61,8 +61,7 @@ export const authOptions = {
   session: {
     strategy: 'jwt',
   },
-  // secret: process.env.NEXTAUTH_SECRET,
-  secret: 'thisisthesecrectauthkey',
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
   },
