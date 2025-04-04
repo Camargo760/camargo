c351ae7f0f781366c499af5f7655f66f26b8aa53
Camargo760 <camargo_co@outlook.com>
2025-04-04 07:36:23 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,16 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useSession, signOut } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+// Import FontAwesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   const { data: session } = useSession()
   const router = useRouter()
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const handleSignOut = async () => {
     const data = await signOut({ redirect: false, callbackUrl: "/" })
@@ -19,11 +25,19 @@ export default function Header() {
         <Link href="/" className="logo">
           <Image src='/assets/logo.png' width={150} height={20} alt="Logo" />
         </Link>
-        <div className="nav-links">
+
+        <button
+          className="hamburger-menu"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+        </button>
+
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
           <Link href="/" className="nav-link">
             Home
           </Link>
-            <Link href="/customOrder" className="nav-link">
+          <Link href="/customOrder" className="nav-link">
             Custom Order
           </Link>
           {session ? (
@@ -49,6 +63,61 @@ export default function Header() {
           )}
         </div>
       </nav>
+
+      <style jsx>{`
+        .nav-container {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding: 10px 20px;
+          position: relative;
+        }
+        
+        .nav-links {
+          display: flex;
+          gap: 60px;
+          text-align: right;
+        }
+
+        .hamburger-menu {
+          display: none;
+          background: none;
+          border: none;
+          cursor: pointer;
+          color: #fff;
+          font-size: 30px;
+        }
+
+        @media (max-width: 650px)  {              
+        .nav-links {
+          gap: 20px;
+           }
+        }
+
+        @media (max-width: 550px) {
+          .nav-links {
+            display: none;
+            position: absolute;
+            top: 90px;
+            right: 0;
+            background: #000;
+            width: 100%;
+            flex-direction: column;
+            align-items: center;
+            gap: 30px;
+            box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+            padding: 40px 0;
+          }
+
+          .nav-links.open {
+            display: flex;
+          }
+
+          .hamburger-menu {
+            display: block;
+          }
+        }
+      `}</style>
     </header>
   )
 }
