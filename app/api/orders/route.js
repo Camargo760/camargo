96ba8c32a86738fa6e9f2edddd8d17996f581804
Camargo760 <camargo_co@outlook.com>
2025-04-05 05:37:46 +0530

    Update route.js

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -5,10 +5,7 @@ import Stripe from "stripe"
 import clientPromise from "../../../lib/mongodb"
 import { ObjectId } from "mongodb"
 
-const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY ||
-  "sk_test_51P2GkSSEzW86D25YTF33BP83Rf4ffGJORl0gfTr3YBvpr5dejYm8bfO6hH3DYBu9saWy9TEDCUELfJNOW1S80rkG00SEhjrTCo",
-)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function GET(request) {
   const session = await getServerSession(authOptions)
