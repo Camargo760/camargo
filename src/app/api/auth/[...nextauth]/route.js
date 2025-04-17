9edc08f51726fb62e170efc9361f7eebfa6e44b4
Camargo760 <camargo_co@outlook.com>
2025-04-17 08:22:44 +0530

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,6 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { compare } from 'bcryptjs';
 import clientPromise from '../../../../lib/mongodb';
-import clientPromise from '../auth-options';
 
 export const authOptions = {
   providers: [
