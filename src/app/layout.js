cfa5928bc0484f09557ca9d5fd90545ecb291f69
Camargo760 <camargo_co@outlook.com>
2025-04-24 14:54:31 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
 'use client';
-import './global.css' // Correct path for global CSS (adjust if needed)
+
+import './global.css';
 import { SessionProvider } from 'next-auth/react';
+import { ClerkProvider } from '@clerk/nextjs';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/assets/sleeve.png" type="image/png" />
-      </head>
-      <body>
-        <SessionProvider>{children}</SessionProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/assets/sleeve.png" type="image/png" />
+        </head>
+        <body>
+          <SessionProvider>
+            {children}
+          </SessionProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
-
