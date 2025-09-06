908268a7f54af9bd3d0c2b09c11f82747c7b7726
Camargo760 <camargo_co@outlook.com>
2025-09-06 15:40:30 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,20 +6,22 @@ import VisitTracker from '@/components/VisitTracker';
 
 export default function RootLayout({ children }) {
   return (
-      <html lang="en">
-        <head>
-          <link rel="icon" href="/assets/sleeve.png" type="image/png" />
-        </head>
-        <body>
-          <SessionProvider>
-            {children}
-            <VisitTracker />
-          </SessionProvider>
-        </body>
-      </html>
+    <html lang="en">
+      <head>
+        {/* Standard favicon */}
+        <link rel="icon" href="/assets/logo.png" type="image/png" />
+
+        {/* Optional but recommended for better support */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/logo.png" />
+      </head>
+      <body>
+        <SessionProvider>
+          {children}
+          <VisitTracker />
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
-
-
-
-
